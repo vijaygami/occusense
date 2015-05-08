5e8be0d9e6b2e9b075066cc747a44c571d391f41
vivek24aggarwal <vivek24aggarwal@gmail.com>
2015-05-08 19:34:53 +0100

    Removing redundant users routing and page.

--- src/server/app.js
+++ src/server/app.js
@@ -7,8 +7,6 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var models = require('./schemas/schemas');
 var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -24,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
