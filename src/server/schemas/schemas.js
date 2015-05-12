4b5ec3150be670223f3b3db01442c961bc162269
peitu <peitu19@gmail.com>
2015-05-12 15:19:11 +0100

    added new schema for database

--- src/server/schemas/schemas.js
+++ src/server/schemas/schemas.js
@@ -1,10 +1,22 @@
 var mongoose = require('mongoose');
 
 // Coord schema 
-var coordSchema = new mongoose.Schema({
+/*var coordSchema = new mongoose.Schema({
 	x: Number,
 	y: Number,
 	z: Number
+})*/
+
+//Gesture Schema
+var gestureSchema = new mongoose.Schema({
+	gestureName: String,
+	time : { type : Date, default: Date.now }
+})
+
+//Activity Schema
+var activitySchema = new mongoose.Schema({
+	activityName: String,
+	time : { type : Date, default: Date.now }
 })
 
 // Sensor Schema
@@ -40,8 +52,42 @@ var appSchema = new mongoose.Schema({
 	}
 })
 
+//People Schema
+var personSchema = new mongoose.Schema({
+	personID: Number,
+	personName: String,
+	//bodypartDim: [Number],
+	roomID: Number,
+	coord:{
+		x: Number,
+		y: Number,
+		z: Number,
+	},
+	activity: {type: String, enum: ['moving','still','lying']},
+})
+
+//Gesture History
+var gesHistorySchema = new mongoose.Schema({
+	personID: Number,
+	personName: String,
+	gesList: [gestureSchema],
+})
+
+//Activity History
+var actHistorySchema = new mongoose.Schema({
+	personID: Number,
+	personName: String,
+	actList: [activitySchema],
+})
+
 var Room = mongoose.model('Room', roomSchema);
 var App = mongoose.model('App', appSchema);
+var Person = mongoose.model('Person',personSchema);
+var GesHistory = mongoose.model('GesHistory',gesHistorySchema);
+var ActHistory = mongoose.model('ActHistory',actHistorySchema);
 
 exports.Room = Room;
 exports.App = App;
+exports.Person = Person;
+exports.GesHistory = GesHistory;
+exports.ActHistory = ActHistory;
