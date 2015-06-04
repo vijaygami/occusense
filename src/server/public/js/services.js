6b9b187b52e68bf1037885e1493ad405548f76f5
vivek24aggarwal <vivek24aggarwal@gmail.com>
2015-06-05 00:06:05 +0100

    Removed redundant index.js. Added live updates to webapp through sockets.

---
+++ src/server/public/js/services.js
@@ -0,0 +1,25 @@
+angular.module('myApp.services', [])
+.factory('socket', function ($rootScope) {
+    var socket = io.connect('http://localhost:3000/webApp');
+    
+    return {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {  
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        }
+    };
+});
