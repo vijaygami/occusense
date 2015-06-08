4dedf5ce5aa29606602cb16b40484ce6f8d709ce
peitu <peitu19@gmail.com>
2015-06-08 13:32:57 +0100

    added sensor event handler to app.js

--- src/server/app.js
+++ src/server/app.js
@@ -193,6 +193,159 @@ ioServer.on('connection', function(socket){
 
 }); // End of ioServer */
 
+//event handler for sensor nodes
+var ioSensor = ioServer.of('/nodes').on('connection',function(socket){
+
+	console.log('Sensor Node connected with id: ' + socket.id);
+	
+	//for debug
+	socket.on('temp',function(){
+		
+		var Person = mongoose.model('Person');
+
+		Person.findOne({personID: 3},function(err,doc){
+			doc.nodeID = socket.id;
+			doc.save();
+			console.log('node is now: ' + doc.nodeID);
+		});
+	});
+
+	//
+	socket.on('disconnect', function(){
+	    console.log('Sensor Node ' + socket.id + ' disconnected!!');
+	});
+
+	//receive JSON and send back / broadcast
+	socket.on('rec_data', function(data){
+		
+		socket.broadcast.emit('res_data', data);
+
+		console.log('Broadcast data from Node ' + socket.id)
+	});
+
+	//
+	socket.on('req_new_ID',function(){
+
+		var Person = mongoose.model('Person');
+
+		//find the number of people and create new ID with count plus 1
+		Person.count({}, function(err, count){
+			
+			var newID = count + 1;
+
+			//create a new person entity
+			var p1 = new Person({
+				personName: 'Guest_' + newID,
+				personID: newID,
+				identified: true,
+				nodeID: socket.id
+
+			});
+
+			p1.save(function(err){
+
+				if(err){
+					return err;
+				}
+				else{
+					console.log('saved ' + p1.personName + ' with ID: ' + p1.personID);
+				}
+
+				//initialise COM and jointPosition for later access
+				Person.findOne({personID: p1.personID},function(err,doc){
+					
+				 	doc.COM.push({});
+					for(i=0;i<15;i++){
+						doc.jointPosition.push({});
+					}
+
+			  		doc.save();
+			  		console.log('person with id: ' + doc);
+				});				
+			});
+
+			socket.emit('res_new_ID', p1.personID);
+
+		});
+
+	});
+
+	//person identified, update database
+	socket.on('identified',function(data){
+
+		console.log('id: ' + data);
+
+		var Person = mongoose.model('Person');
+		Person.findOne({personID: data},function(err,doc){
+			var name = doc.personName;
+			console.log('person identified ' + name);
+			doc.identified = true;
+		});
+	});
+
+	//updating COM and jointPosition from node
+	socket.on('person_COM',function(data){
+
+		var Person = mongoose.model('Person');
+
+
+		var size = Object.keys(data).length;
+
+		for (i = 0; i < size; i++){
+    		
+    		pID = data[i].id;
+
+    		console.log('Updating COM and jointPosition for person with id: ' + pID);
+
+    		var k = i;
+
+			Person.findOne({personID: pID},function(err,doc){
+
+				//update COM field
+				doc.COM[0].cx = data[k].COM[0];
+				doc.COM[0].cy = data[k].COM[1];
+				doc.COM[0].cz = data[k].COM[2];
+				
+				for(j = 0; j < 15; j++){
+
+					//update jointPosition
+					doc.jointPosition[j].cx = data[k].joint[j][0];
+					doc.jointPosition[j].cy = data[k].joint[j][1];
+					doc.jointPosition[j].cz = data[k].joint[j][2];
+				}
+    			
+    		 	doc.save();
+
+    		 	console.log(doc);
+    		});
+		}
+	});
+	
+	//event for gesture performed
+	socket.on('ges_perf',function(gID, pID){
+		
+		console.log(gID + ' ' + pID);
+
+		var Person = mongoose.model('Person');
+
+		Person.findOne({personID: pID},function(err,doc){
+
+			//push new gesture record to history log
+    		doc.gesList.push({gesID:gID});
+
+    		doc.save();
+
+    	});
+	});
+
+	//
+	socket.on('ges_new',function(data, gID){
+		
+		//after training, forward 
+		socket.broadcast.emit('ges_res',data, gID);
+	})
+
+});
 
 // Watch people collection in depthdb for changes
 var oplog = new OplogWatcher({
